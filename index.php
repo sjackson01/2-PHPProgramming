b0279c7b7ee0bcef0dec0b10f9a8f15fc559d70e
Steven Jackson <stevenpjackson0@gmail.com>
2020-02-04 14:28:28 -0600

    Initial Commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Simple HTML Forms</title>
+</head>
+<header>
+    <a href="Link.php">Constants</a>
+</header>
+<body>
+    
+</body>    
+</html> 
